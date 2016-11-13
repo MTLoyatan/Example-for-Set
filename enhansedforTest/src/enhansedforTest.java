fbd69ec42cc6275ac35b1c1a9fdfdc1fcb726347
Mehdi taheri <Mehdi taheri>
2016-11-13 07:17:52 -0500

    example of enhanced for loop

---
+++ enhansedforTest/src/enhansedforTest.java
@@ -0,0 +1,18 @@
+
+public class enhansedforTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array={12,10,13,14,15,16,17,18,19,20,22};
+		int  total=0;
+		
+		for(int number : array)
+			total+=number;
+			System.out.printf(" th total is :%d\n",total);
+		
+	}
+
+}
